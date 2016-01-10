affd791a99493cc97a16f50c8929714639428d68
Finn Voichick <fvoichick@gmail.com>
2016-01-10 08:35:04 -0600

    Added a straight-driving option for DriveTank

--- src/org/usfirst/frc/team1306/robot/commands/DriveTank.java
+++ src/org/usfirst/frc/team1306/robot/commands/DriveTank.java
@@ -15,7 +15,8 @@ public class DriveTank extends CommandBase {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		drivetrain.driveTank(oi.getLeft(), oi.getRight());
+		double straightVel = oi.getRightTrigger() - oi.getLeftTrigger();
+		drivetrain.driveTank(oi.getLeft() + straightVel, oi.getRight() + straightVel);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
