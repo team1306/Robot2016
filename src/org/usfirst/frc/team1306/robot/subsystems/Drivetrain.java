42af6209fde0dfcf51a8feb5d3b0396048879841
Finn Voichick <fvoichick@gmail.com>
2016-01-10 09:48:53 -0600

    Motors on the drivetrain are now controlled individually

--- src/org/usfirst/frc/team1306/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Drivetrain.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1306.robot.subsystems;
 import org.usfirst.frc.team1306.robot.RobotMap;
 import org.usfirst.frc.team1306.robot.commands.DriveTank;
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -11,8 +12,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drivetrain extends Subsystem {
 
-	private RobotDrive drivetrain = new RobotDrive(RobotMap.leftTalon1Port, RobotMap.leftTalon2Port,
-			RobotMap.rightTalon1Port, RobotMap.rightTalon2Port);
+	private CANTalon leftMotor1 = new CANTalon(RobotMap.leftTalon1Port);
+	private CANTalon leftMotor2 = new CANTalon(RobotMap.leftTalon2Port);
+	private CANTalon rightMotor1 = new CANTalon(RobotMap.rightTalon1Port);
+	private CANTalon rightMotor2 = new CANTalon(RobotMap.rightTalon1Port);
+
+	private RobotDrive drivetrain = new RobotDrive(leftMotor1, leftMotor2, rightMotor1, rightMotor2);
 
 	public void driveTank(double leftVel, double rightVel) {
 		drivetrain.tankDrive(leftVel * MAX_SPEED, rightVel * MAX_SPEED);
@@ -29,7 +34,7 @@ public class Drivetrain extends Subsystem {
 	public void initDefaultCommand() {
 		setDefaultCommand(new DriveTank());
 	}
-	
+
 	/** The max speed, in encoder ticks/10ms. Currently a placeholder value. */
 	private static double MAX_SPEED = 1.0;
 }
