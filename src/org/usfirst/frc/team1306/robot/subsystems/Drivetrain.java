7dd3a64cc0abf4cdf53384fe38e4662a480d58ae
Finn Voichick <fvoichick@gmail.com>
2016-01-10 10:32:34 -0600

    Added PID constants

--- src/org/usfirst/frc/team1306/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Drivetrain.java
@@ -12,12 +12,27 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Drivetrain extends Subsystem {
 
-	private CANTalon leftMotor1 = new CANTalon(RobotMap.leftTalon1Port);
-	private CANTalon leftMotor2 = new CANTalon(RobotMap.leftTalon2Port);
-	private CANTalon rightMotor1 = new CANTalon(RobotMap.rightTalon1Port);
-	private CANTalon rightMotor2 = new CANTalon(RobotMap.rightTalon1Port);
+	private final CANTalon leftMotor1;
+	private final CANTalon leftMotor2;
+	private final CANTalon rightMotor1;
+	private final CANTalon rightMotor2;
 
-	private RobotDrive drivetrain = new RobotDrive(leftMotor1, leftMotor2, rightMotor1, rightMotor2);
+	private final RobotDrive drivetrain;
+
+	public Drivetrain() {
+
+		leftMotor1 = new CANTalon(RobotMap.leftTalon1Port);
+		leftMotor2 = new CANTalon(RobotMap.leftTalon2Port);
+		rightMotor1 = new CANTalon(RobotMap.rightTalon1Port);
+		rightMotor2 = new CANTalon(RobotMap.rightTalon1Port);
+
+		drivetrain = new RobotDrive(leftMotor1, leftMotor2, rightMotor1, rightMotor2);
+
+		leftMotor1.setPID(P, I, D, F, IZONE, CLOSED_LOOP_RAMP_RATE, 0);
+		leftMotor2.setPID(P, I, D, F, IZONE, CLOSED_LOOP_RAMP_RATE, 0);
+		rightMotor1.setPID(P, I, D, F, IZONE, CLOSED_LOOP_RAMP_RATE, 0);
+		rightMotor2.setPID(P, I, D, F, IZONE, CLOSED_LOOP_RAMP_RATE, 0);
+	}
 
 	public void driveTank(double leftVel, double rightVel) {
 		drivetrain.tankDrive(leftVel * MAX_SPEED, rightVel * MAX_SPEED);
@@ -35,6 +50,13 @@ public class Drivetrain extends Subsystem {
 		setDefaultCommand(new DriveTank());
 	}
 
-	/** The max speed, in encoder ticks/10ms. Currently a placeholder value. */
+	/** All of these are placeholder values. */
 	private static double MAX_SPEED = 1.0;
+	private static double P = 1.0;
+	private static double I = 0.0;
+	private static double D = 0.0;
+	private static double F = 0.0;
+	private static int IZONE = 0;
+	private static double CLOSED_LOOP_RAMP_RATE = 10.0;
+
 }
