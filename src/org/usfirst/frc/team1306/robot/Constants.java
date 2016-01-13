7d77ebaf8cd6599b0c4761d6f443fb7dced10dbe
James Tautges <jamesdtautges@gmail.com>
2016-01-13 10:29:53 -0600

    Moved some drivetrain constants to the Constants class

--- src/org/usfirst/frc/team1306/robot/Constants.java
+++ src/org/usfirst/frc/team1306/robot/Constants.java
@@ -2,5 +2,5 @@ package org.usfirst.frc.team1306.robot;
 
 public class Constants {
 	public final static double MAX_ACCELERATION = 0.01; // measured in encoder ticks per 1ms^2
-	public final static double MAX_SPEED = 850;
+	public final static double MAX_SPEED = 850.0;
 }
