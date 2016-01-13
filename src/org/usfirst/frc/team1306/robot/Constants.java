fdf397e181aa797e8a81f1fe3ba8feab69678be7
James Tautges <jamesdtautges@gmail.com>
2016-01-13 09:58:19 -0600

    Added constants class to contain magic numbers

---
+++ src/org/usfirst/frc/team1306/robot/Constants.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc.team1306.robot;
+
+public class Constants {
+	public final static double MAX_ACCELERATION = 0.01; // measured in encoder ticks per 1ms^2
+	public final static double MAX_SPEED = 850;
+}
