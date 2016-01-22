83a4ad97084cd5832a87136d44392111632f4593
Finn Voichick <fvoichick@gmail.com>
2016-01-21 19:59:27 -0600

    AutoTarget uses the new Vision structure

--- src/org/usfirst/frc/team1306/robot/commands/turret/AutoTarget.java
+++ src/org/usfirst/frc/team1306/robot/commands/turret/AutoTarget.java
@@ -1,37 +1,47 @@
 package org.usfirst.frc.team1306.robot.commands.turret;
 
 import org.usfirst.frc.team1306.robot.commands.CommandBase;
+import org.usfirst.frc.team1306.robot.subsystems.vision.VisionData;
+
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  *
  */
 public class AutoTarget extends CommandBase {
 
-    public AutoTarget() {
-        requires(turret);
-    }
+	private double recentTimestamp;
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public AutoTarget() {
+		requires(turret);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		turret.setTargetRelative(vision.getData().getYaw());
+		recentTimestamp = vision.getData().getTimestamp();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return turret.onTarget();
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (vision.getData().getTimestamp() > recentTimestamp) {
+			turret.setTargetRelative(vision.getData().getYaw());
+		}
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    	turret.stop();
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return turret.onTarget();
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		turret.stop();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
