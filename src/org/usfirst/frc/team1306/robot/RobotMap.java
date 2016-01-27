5d905eb87a217d3604c584a495674596814ec3ca
James Tautges <jamesdtautges@gmail.com>
2016-01-27 14:51:24 -0600

    Categorized the different port numbers

--- src/org/usfirst/frc/team1306/robot/RobotMap.java
+++ src/org/usfirst/frc/team1306/robot/RobotMap.java
@@ -8,9 +8,11 @@ package org.usfirst.frc.team1306.robot;
  */
 public class RobotMap {
 
+	// oi ports
 	public static int xboxPort = 0;
 	public static int secondaryPort = 1;
 
+	// drivetrain controllers ports
 	public static int leftTalon1Port = 1;
 	public static int leftTalon2Port = 2;
 	public static int leftTalon3Port = 3;
@@ -18,11 +20,11 @@ public class RobotMap {
 	public static int rightTalon2Port = 5;
 	public static int rightTalon3Port = 6;
 	
+	// shooter and turret controller ports
 	public static int flyWheelTalonPort = 7;
 	public static int hoodTalonPort = 8;
 	public static int turretTalonPort = 9;
 	
-	public static int turretEncoderPort = 2;
-	
+	// intake sensor ports
 	public static int intakeLimitPort = 1;
 }
