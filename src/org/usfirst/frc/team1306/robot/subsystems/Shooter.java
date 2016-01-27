d2b6207efc7d6b91b65cadb1a8a3de8d62ef14d7
James Tautges <jamesdtautges@gmail.com>
2016-01-27 14:32:57 -0600

    Commented shooter subsystem

--- src/org/usfirst/frc/team1306/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Shooter.java
@@ -12,41 +12,64 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Shooter extends Subsystem {
-    
-    private CANTalon flywheel;
-    
-    private double target = 0;
-    
-    public Shooter() {
-    	flywheel = new CANTalon(RobotMap.flyWheelTalonPort);
-    	
-    	flywheel.reverseOutput(true);
-    	flywheel.reverseSensor(true);
-    	
-    	flywheel.setFeedbackDevice(FeedbackDevice.QuadEncoder);
-    	flywheel.changeControlMode(TalonControlMode.Speed);
-    	flywheel.enable();
-    }
 
-    public void initDefaultCommand() {
-    }
-    
-    public void spinTo(double speed) {
-    	flywheel.set(speed);
-    	target = speed;
-    }
-    
-    public void spinUp() {
-    	spinTo(1.0);
-    }
-    
-    public void spinDown() {
-    	spinTo(0.0);
-    }
-    
-    public boolean isSpunUp() {
-    	return Math.abs(flywheel.getEncVelocity() - target) < Constants.HOOD_TOLERANCE;
-    }
-    
+	private CANTalon flywheel;
+
+	private double target = 0;
+
+	public Shooter() {
+		flywheel = new CANTalon(RobotMap.flyWheelTalonPort);
+
+		flywheel.reverseOutput(true);
+		flywheel.reverseSensor(true);
+
+		flywheel.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+		flywheel.changeControlMode(TalonControlMode.Speed);
+		flywheel.enable();
+	}
+
+	public void initDefaultCommand() {
+	}
+
+	/**
+	 * Set the target speed for the flywheel Talon. This value should be in
+	 * ticks per 10ms
+	 * 
+	 * @param speed
+	 *            Speed to set the Talon
+	 */
+
+	public void spinTo(double speed) {
+		flywheel.set(speed);
+		target = speed;
+	}
+
+	/**
+	 * Set the Talon to full speed
+	 */
+
+	public void spinUp() {
+		spinTo(1.0);
+	}
+
+	/**
+	 * Stop the flywheel Talon
+	 */
+
+	public void spinDown() {
+		spinTo(0.0);
+	}
+
+	/**
+	 * Return whether or not the measured speed is within tolerance of our
+	 * target value. This means we can fire when ready
+	 * 
+	 * @return Whether or not the measured speed is within tolerance of the
+	 *         target
+	 */
+
+	public boolean isSpunUp() {
+		return Math.abs(flywheel.getEncVelocity() - target) < Constants.HOOD_TOLERANCE;
+	}
+
 }
-
