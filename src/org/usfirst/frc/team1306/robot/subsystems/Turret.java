1c21137a9aa98bd781da46e00f8bfe8eddc20c51
James Tautges <jamesdtautges@gmail.com>
2016-01-27 14:36:44 -0600

    Commented turret subsystem

--- src/org/usfirst/frc/team1306/robot/subsystems/Turret.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Turret.java
@@ -17,15 +17,16 @@ public class Turret extends Subsystem {
 	private final CANTalon turretTalon;
 	private final CANTalon hoodTalon;
 
-	// Initialize your subsystem here
 	public Turret() {
 
+		// Configure the turret Talon with an encoder and position control
 		turretTalon = new CANTalon(RobotMap.turretTalonPort);
 		turretTalon.setFeedbackDevice(FeedbackDevice.QuadEncoder);
 		turretTalon.changeControlMode(TalonControlMode.Position);
 		turretTalon.set(turretTalon.get());
 		turretTalon.enable();
 
+		// Configure the hood Talon with an encoder and position control
 		hoodTalon = new CANTalon(RobotMap.hoodTalonPort);
 		hoodTalon.setFeedbackDevice(FeedbackDevice.QuadEncoder);
 		hoodTalon.changeControlMode(TalonControlMode.Position);
@@ -44,30 +45,62 @@ public class Turret extends Subsystem {
 	 * @param velocity
 	 *            the new velocity
 	 */
+
 	public void setVel(double velocity) {
 		turretTalon.changeControlMode(TalonControlMode.Speed);
 		turretTalon.set(velocity * Constants.TURRET_MAX_SPEED);
 	}
+
+	/**
+	 * Set the target position of the turret measured in encoder ticks
+	 * 
+	 * @param position
+	 *            Target position in encoder ticks
+	 */
 
 	public void setTarget(double position) {
 		turretTalon.changeControlMode(TalonControlMode.Position);
 		turretTalon.set(position);
 	}
 
+	/**
+	 * Set the target relative angle from the current angle.
+	 * 
+	 * @param angle
+	 *            Target relative angle
+	 */
+
 	public void setTargetRelative(double angle) {
 		turretTalon.changeControlMode(TalonControlMode.Position);
 		turretTalon.set(turretTalon.get() + angle);
 	}
+
+	/**
+	 * Stop the turret
+	 */
 
 	public void stop() {
 		turretTalon.changeControlMode(TalonControlMode.Position);
 		turretTalon.set(turretTalon.get());
 	}
 
+	/**
+	 * Return whether or not the turret is within tolerance of our target angle
+	 * 
+	 * @return Whether or not the turret is on target
+	 */
+
 	public boolean onTarget() {
 		return turretTalon.getControlMode().equals(TalonControlMode.Position)
 				&& turretTalon.getError() < Constants.TURRET_TOLERANCE;
 	}
+
+	/**
+	 * Set the target position of the hood
+	 * 
+	 * @param position
+	 *            Position of the hood in encoder ticks
+	 */
 
 	public void setHoodHeight(double position) {
 		hoodTalon.set(position);
