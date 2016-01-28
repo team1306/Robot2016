d8647f41a34b517f2d97ab50947b0cdb3696b4c5
James Tautges <jamesdtautges@gmail.com>
2016-01-28 09:19:55 -0600

    Added basic subsystem methods to control the intake

--- src/org/usfirst/frc/team1306/robot/subsystems/Intake.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Intake.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team1306.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -15,8 +16,8 @@ public class Intake extends Subsystem {
     private final CANTalon leftIntakeMotor;
     private final CANTalon rightIntakeMotor;
     
-    private final Victor roller1;
-    private final Victor roller2;
+    private final Talon roller1;
+    private final Talon roller2;
     
     private final DoubleSolenoid leftIntakeSol;
     private final DoubleSolenoid rightIntakeSol;
@@ -25,8 +26,8 @@ public class Intake extends Subsystem {
     	leftIntakeMotor = new CANTalon(RobotMap.intakeMotor1Port);
     	rightIntakeMotor = new CANTalon(RobotMap.intakeMotor2Port);
     	
-    	roller1 = new Victor(RobotMap.intakeRoller1Port);
-    	roller2 = new Victor(RobotMap.intakeRoller2Port);
+    	roller1 = new Talon(RobotMap.intakeRoller1Port);
+    	roller2 = new Talon(RobotMap.intakeRoller2Port);
     	
     	leftIntakeSol = new DoubleSolenoid(RobotMap.intakeSol1PortA, RobotMap.intakeSol1PortB);
     	rightIntakeSol = new DoubleSolenoid(RobotMap.intakeSol2PortA, RobotMap.intakeSol2PortB);
@@ -35,6 +36,42 @@ public class Intake extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    /**
+     * Set the rollers spinning
+     */
+    
+    public void startRollers() {
+    	roller1.set(1.0);
+    	roller2.set(1.0);
+    }
+    
+    /**
+     * Stop the rollers spinning
+     */
+    
+    public void stopRollers() {
+    	roller1.set(0.0);
+    	roller2.set(0.0);
+    }
+    
+    /**
+     * Actuate the solenoids to extend the tusks
+     */
+    
+    public void extendTusks() {
+    	leftIntakeSol.set(DoubleSolenoid.Value.kForward);
+    	rightIntakeSol.set(DoubleSolenoid.Value.kForward);
+    }
+    
+    /**
+     * Actuate the solenoids to retract the tusks
+     */
+    
+    public void retractTusks() {
+    	leftIntakeSol.set(DoubleSolenoid.Value.kReverse);
+    	rightIntakeSol.set(DoubleSolenoid.Value.kReverse);
     }
 }
 
