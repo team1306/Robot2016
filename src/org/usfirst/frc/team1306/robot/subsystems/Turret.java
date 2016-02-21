6e2e9e7321513f15a05c6fec8c72ac84360fb211
James Tautges <jamesdtautges@gmail.com>
2016-02-20 18:42:43 -0600

    Removed old import

--- src/org/usfirst/frc/team1306/robot/subsystems/Turret.java
+++ src/org/usfirst/frc/team1306/robot/subsystems/Turret.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team1306.robot.subsystems;
 
 import org.usfirst.frc.team1306.robot.Constants;
 import org.usfirst.frc.team1306.robot.RobotMap;
-import org.usfirst.frc.team1306.robot.commands.turret.TurretPIDHelp;
 import org.usfirst.frc.team1306.robot.vision.Vision;
 
 import edu.wpi.first.wpilibj.CANTalon;
