6d0d95b1eaa668099b2bd10059727d6ac3782b16
Finn Voichick <fvoichick@gmail.com>
2016-04-17 10:47:10 -0400

    SmartDashboardUpdate documentation

--- src/org/usfirst/frc/team1306/robot/commands/SmartDashboardUpdate.java
+++ src/org/usfirst/frc/team1306/robot/commands/SmartDashboardUpdate.java
@@ -7,26 +7,39 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- *
+ * A command that keeps the SmartDashboard updated, used both for debugging and
+ * getting information to the drivers.
+ * 
+ * @author Finn Voichick, James Tautges
  */
 public class SmartDashboardUpdate extends CommandBase {
 
+	/**
+	 * Constructs a new SmartDashboardUpdate command. This command is started
+	 * when the robot is initialized, so to prevent it from being interupted
+	 * when the robot goes into disabled mode, this flag needs to be set.
+	 */
 	public SmartDashboardUpdate() {
-		// This command is started when the robot is initialized, so to prevent
-		// it from being interupted when the robot goes into disabled mode, this
-		// flag needs to be set
 		setRunWhenDisabled(true);
 	}
 
+	/**
+	 * Called just before this Command runs the first time. Nothing is here
+	 * because everything needs to happen repeatedly.
+	 */
 	protected void initialize() {
 	}
 
-	// Print various debugging values to the SmartDashboard. These commands can
-	// invoke any methods of any subsystem provided they're read only
-	// operations. Since we don't require any subsystems, we can't guarantee
-	// that other commands aren't accessing it
+	/**
+	 * Print various debugging values to the SmartDashboard. These commands can
+	 * invoke any methods of any subsystem provided they're read only
+	 * operations. Since we don't require any subsystems, we can't guarantee
+	 * that other commands aren't accessing it. Capitalized SmartDashboard
+	 * values are typically for the drivers, while lowercase values are
+	 * typically for debugging purposes.
+	 */
 	protected void execute() {
-		// Values useful for PID debugging
+
 		SmartDashboard.putNumber("Game Time", Math.floor(Timer.getMatchTime()));
 
 		SmartDashboard.putNumber("hood angle", hood.getHeight());
@@ -67,17 +80,40 @@ public class SmartDashboardUpdate extends CommandBase {
 
 	}
 
+	/**
+	 * Make this return true when this Command no longer needs to run execute().
+	 * This command only ends when the robot is powered off (or the code dies).
+	 * 
+	 * @return false
+	 */
+	@Override
 	protected boolean isFinished() {
 		return false;
 	}
 
+	/**
+	 * Called once after isFinished returns true. This command never does end.
+	 */
 	protected void end() {
 	}
 
+	/**
+	 * Called when another command which requires one or more of the same
+	 * subsystems is scheduled to run. This command requires no subsystems, so
+	 * it is never interrupted.
+	 */
 	protected void interrupted() {
 	}
 
+	/**
+	 * Converts a double value in feet to a human-friendly String, in feet and
+	 * inches.
+	 * 
+	 * @param feet
+	 *            a value in feet.
+	 * @return a more readable String.
+	 */
 	private static String displayFeet(double feet) {
-		return Math.floor(feet + 1.0/24.0) + " ft " + Math.floor(feet * 12 + 0.5) % 12 + " in";
+		return Math.floor(feet + 1.0 / 24.0) + " ft " + Math.floor(feet * 12 + 0.5) % 12 + " in";
 	}
 }
